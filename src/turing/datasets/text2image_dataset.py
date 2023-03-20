1cdda1f26ca692f0ffa78c7df23c622df8b86b81
Toan Do <toan.do@stochastic.ai>
2023-03-20 19:36:45 +0700

    feat: add base classes for turing library

---
+++ src/turing/datasets/text2image_dataset.py
@@ -0,0 +1,12 @@
+from typing import Union
+from pathlib import Path
+
+class Text2ImageDataset:
+    def __init__(
+        self, 
+        path: Union[str, Path]
+    ):
+        pass
+    
+    def _validate(self):
+        pass
