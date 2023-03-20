7a41de2f0d0c74879d9877b24abf9c5cf6dbbea4
Toan Do <toan.do@stochastic.ai>
2023-03-20 19:36:45 +0700

    feat: add base classes for turing library

---
+++ src/turing/datasets/text_dataset.py
@@ -0,0 +1,12 @@
+from typing import Union
+from pathlib import Path
+
+class TextDataset:
+    def __init__(
+        self, 
+        path: Union[str, Path]
+    ):
+        pass
+    
+    def _validate(self):
+        pass
---
+++ src/turing/models/gptj.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import List, Union, Optional
+from turing.datasets.text_dataset import TextDataset
+from turing.datasets.instruction_dataset import InstructionDataset
+
+
+class GPTJ:
+    def __init__(self, weights_path: str):
+        pass
+    
+    def finetune(
+        self,
+        dataset: Union[TextDataset, InstructionDataset]
+    ):
+        pass
+
+    def evaluate(
+        self,
+        dataset: Union[TextDataset, InstructionDataset]
+    ):
+        pass
+
+    def generate(
+        self,
+        texts: Optional[Union[List[str], str]] = None,
+        dataset: Optional[Union[TextDataset, InstructionDataset]] = None
+    ):
+        pass
+
+    def save(
+        self,
+        path: Union[str, Path]
+    ):
+        pass
