ef90b7ea904b895440a4df75b9548954e4c794f1
Marcos Rivera Martinez <marcos.rm@stochastic.ai>
2023-03-24 11:49:21 +0100

    fix: remove incorrect import - LlamaTokenizer
    Remove incorrect import

--- src/xturing/engines/llama_engine.py
+++ src/xturing/engines/llama_engine.py
@@ -9,7 +9,6 @@ import torch
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss
-from transformers import LlamaTokenizer
 from transformers.activations import ACT2FN
 from transformers.configuration_utils import PretrainedConfig
 from transformers.modeling_outputs import (
