cbd76d0ef6a2df066f44bbcd319bee4708344131
Marcos Rivera Martinez <marcos.rm@stochastic.ai>
2023-03-27 18:24:28 +0200

    fix: replace distilgpt2 by gpt2
    replace distilgpt2 by gpt2

--- src/xturing/engines/gpt2_engine.py
+++ src/xturing/engines/gpt2_engine.py
@@ -8,7 +8,7 @@ class GPT2Engine(CausalEngine):
     config_name: str = "gpt2_engine"
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
-        super().__init__(model_name="distilgpt2", weights_path=weights_path)
+        super().__init__(model_name="gpt2", weights_path=weights_path)
 
         self.tokenizer.pad_token = self.tokenizer.eos_token
 
@@ -17,6 +17,6 @@ class GPT2LoraEngine(CausalLoraEngine):
     config_name: str = "gpt2_lora_engine"
 
     def __init__(self, weights_path: Optional[Union[str, Path]] = None):
-        super().__init__(model_name="distilgpt2", weights_path=weights_path)
+        super().__init__(model_name="gpt2", weights_path=weights_path)
 
         self.tokenizer.pad_token = self.tokenizer.eos_token
