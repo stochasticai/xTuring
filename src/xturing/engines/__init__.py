71f93daf764a851d0611121532ebad9f7a5f3215
Marcos Rivera Martinez <marcos.rm@stochastic.ai>
2023-03-27 18:25:48 +0200

    fix: add distilgpt2 to registry
    add distilgpt2 to registry

--- src/xturing/engines/__init__.py
+++ src/xturing/engines/__init__.py
@@ -1,8 +1,11 @@
 from .base import BaseEngine
+from .distilgpt2_engine import DistilGPT2Engine, DistilGPT2LoraEngine
 from .gpt2_engine import GPT2Engine, GPT2LoraEngine
 from .gptj_engine import GPTJEngine, GPTJLoraEngine
 from .llama_engine import LLamaEngine, LlamaLoraEngine
 
+BaseEngine.add_to_registry(DistilGPT2Engine.config_name, DistilGPT2Engine)
+BaseEngine.add_to_registry(DistilGPT2LoraEngine.config_name, DistilGPT2LoraEngine)
 BaseEngine.add_to_registry(GPTJEngine.config_name, GPTJEngine)
 BaseEngine.add_to_registry(GPTJLoraEngine.config_name, GPTJLoraEngine)
 BaseEngine.add_to_registry(LLamaEngine.config_name, LLamaEngine)
