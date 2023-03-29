00a4412b9318d29b83c01be070223697c6091b9b
Toan Do <toan.do@stochastic.ai>
2023-03-29 21:47:23 +0700

    feat: add galactica, opt, cerebras models
    Add Galactica, OPT, Cerebras models and examples

--- examples/opt/opt_lora_int8.py
+++ examples/opt/opt_lora_int8.py
@@ -1,15 +1,15 @@
 from xturing.datasets.instruction_dataset import InstructionDataset
 from xturing.models import BaseModel
 
-instruction_dataset = InstructionDataset("./alpaca_data")
+instruction_dataset = InstructionDataset("../llama/alpaca_data")
 # Initializes the model
-model = BaseModel.create("llama_lora_int8")
+model = BaseModel.create("opt_lora_int8")
 # Finetuned the model
 model.finetune(dataset=instruction_dataset)
 # Once the model has been finetuned, you can start doing inferences
 output = model.generate(texts=["Why LLM models are becoming so important?"])
 print("Generated output by the model: {}".format(output))
 # Save the model
-model.save("./llama_weights")
+model.save("./opt_weights")
 
 # If you want to load the model just do BaseModel.load("./llama_weights")
