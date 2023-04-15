07bd450e3fee2f136f0f4a3f0ad1525161ae9f43
Toan Do <toan.do@stochastic.ai>
2023-04-15 17:58:26 +0700

    feat: save and load only lora params
    add saving and loading only LoRA parameters

--- examples/gptj/gptj_lora_int8.py
+++ examples/gptj/gptj_lora_int8.py
@@ -1,11 +1,15 @@
 from xturing.datasets.instruction_dataset import InstructionDataset
 from xturing.models import BaseModel
 
-instruction_dataset = InstructionDataset("../llama/alpaca_data")
+instruction_dataset = InstructionDataset("../llama/alpaca_data2")
 # Initializes the model
 model = BaseModel.create("gptj_lora_int8")
 # Finetuned the model
 model.finetune(dataset=instruction_dataset)
+
+# Save the model
+model.save("./gptj_weights")
+
 # Once the model has been finetuned, you can start doing inferences
 output = model.generate(texts=["Why LLM models are becoming so important?"])
 print("Generated output by the model: {}".format(output))
