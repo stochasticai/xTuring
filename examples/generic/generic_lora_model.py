21f73751489ba9c38295d8e287cdda7297572cdd
Tushar <tushar.mohan2001@gmail.com>
2023-06-12 13:34:21 +0000

    fix: typo in examples/ repo
    Fixed a typo in generic_lora_model.py file. Had not import Lora version

--- examples/generic/generic_lora_model.py
+++ examples/generic/generic_lora_model.py
@@ -1,9 +1,9 @@
 from xturing.datasets.instruction_dataset import InstructionDataset
-from xturing.models import GenericModel
+from xturing.models import GenericLoraModel
 
 instruction_dataset = InstructionDataset("./alpaca_data")
 # Initializes the model
-model = GenericModel("facebook/opt-1.3b", target_modules=["q_proj", "v_proj"])
+model = GenericLoraModel("facebook/opt-1.3b", target_modules=["q_proj", "v_proj"])
 # Finetuned the model
 model.finetune(dataset=instruction_dataset)
 # Once the model has been finetuned, you can start doing inferences
