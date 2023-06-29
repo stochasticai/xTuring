a6833f225dfe3aca9252eb414f5a2f7230f35bd4
Mahesh Sinha <31616939+msinha251@users.noreply.github.com>
2023-06-29 12:49:15 +0200

    Update generic_engine.py

--- src/xturing/engines/generic_engine.py
+++ src/xturing/engines/generic_engine.py
@@ -13,11 +13,13 @@ class GenericEngine(CausalEngine):
         model_name: str,
         weights_path: Optional[Union[str, Path]] = None,
         trust_remote_code: Optional[bool] = False,
+        **kwargs,
     ):
         super().__init__(
             model_name=model_name,
             weights_path=weights_path,
             trust_remote_code=trust_remote_code,
+            **kwargs,
         )
 
         self.tokenizer.pad_token = self.tokenizer.eos_token
@@ -32,14 +34,13 @@ class GenericLoraEngine(CausalLoraEngine):
         target_modules: List[str],
         weights_path: Optional[Union[str, Path]] = None,
         trust_remote_code: Optional[bool] = False,
-        **kwargs,
+        
     ):
         super().__init__(
             model_name=model_name,
             weights_path=weights_path,
             target_modules=target_modules,
             trust_remote_code=trust_remote_code,
-            **kwargs,
         )
 
         self.tokenizer.pad_token = self.tokenizer.eos_token
