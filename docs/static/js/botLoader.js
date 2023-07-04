8eb5ad6f9fe4c796ff5e9c4632bd97689944bf21
Subhash G N <subhash.gn@stochastic.ai>
2023-07-05 02:41:57 +0530

    fix: embed scripts
    Updated embed scripts

--- docs/static/js/botLoader.js
+++ docs/static/js/botLoader.js
@@ -1,1 +1,2 @@
-window.xChatEmbed="https://xchat-dev.stochastic.ai/embed?xchat_id=6499bceaba648d30e0a8e374&user_id=6499bb32ed2865161ccd25c5"
+window.xChatEmbed="https://xchat-dev.stochastic.ai/embed?xchat_id=64a4490273968e746a6e67a6&user_id=6499bb32ed2865161ccd25c5"
+x
