4521aeb5da2e07185e2ea6b3480fed61fcf7b5ce
Tushar <tushar.mohan2001@gmail.com>
2023-08-17 11:17:48 +0530

    revamped dropdowns in finetune guide

--- docs/docs/finetune/test.jsx
+++ docs/docs/finetune/test.jsx
@@ -1,46 +1,86 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import clsx from 'clsx'
 import MDXContent from '@theme/MDXContent'
 import CodeBlock from '@theme/CodeBlock'
 
+const trainingTechniques = {
+  base: 'Base',
+  lora: 'LoRa',
+  lora_int8: 'LoRA INT8',
+  int8: 'INT8',
+}
+
+const modelList = {
+  bloom: 'BLOOM',
+  cerebras: 'Cerebras',
+  distilgpt2: 'DistilGPT-2',
+  galactica: 'Galactica',
+  gptj: 'GPT-J', 
+  gpt2: 'GPT-2',
+  llama: 'LLaMA',
+  llama2: 'LLaMA 2',
+  opt: 'OPT',
+}
+
 export default function Test(
   {instruction}
 ) {
-  const [code, setCode] = useState('llama');
+  // const [code, setCode] = useState('llama');
+  const [code, setCode] = useState({
+    model: '',
+    technique: 'base',
+  })
+
+  let finalKey = ''
+  if (code.technique === 'base') {
+    finalKey = `${code.model}`
+  } else {
+    finalKey = `${code.model}_${code.technique}`
+  }
+  
+  useEffect(() => {
+    setCode({
+      model: 'bloom',
+      technique: 'base'
+    });
+  }, []);
 
   return (
     <div className={clsx('col')}>
       <label htmlFor='model'>Choose a model: </label>
-
       <select
+        style={{ padding: '8px 16px', borderRadius: '8px', marginRight:'5px' }}
         name='model'
         id='model'
-        onChange={(e) => setCode(e.target.value)}
+        onChange={(e) =>
+          setCode((prev) => ({
+            ...prev,
+            model: e.target.value,
+          }))
+        }
       >
-        <option value='llama'>LLaMA</option>
-        <option value='llama_lora'>LLaMA LoRa</option>
-        <option value='llama_lora_int8'>LLaMA LoRA INT8 </option>
-        <option value='gptj'>GPT-J</option>
-        <option value='gptj_lora'>GPT-J LoRA</option>
-        <option value='gptj_lora_int8'>GPT-J LoRA INT8</option>
-        <option value='gpt2'>GPT-2</option>
-        <option value='gpt2_lora'>GPT-2 LoRA </option>
-        <option value='gpt2_lora_int8'>GPT-2 LoRA INT8 </option>
-        <option value='distilgpt2'>DistilGPT-2 </option>
-        <option value='distilgpt2_lora'>DistilGPT-2 LoRA</option>
-        <option value='opt'>OPT</option>
-        <option value='opt_lora'>OPT LoRA</option>
-        <option value='opt_lora_int8'>OPT LoRA INT8</option>
-        <option value='cerebras'>Cerebras </option>
-        <option value='cerebras_lora'>Cerebras LoRA </option>
-        <option value='cerebras_lora_int8'>Cerebras LoRA INT8 </option>
-        <option value='galactica'>Galactica </option>
-        <option value='galactica_lora'>Galactica LoRA </option>
-        <option value='galactica_lora_int8'>Galactica LoRA INT8 </option>
-        <option value='bloom'>BLOOM</option>
-        <option value='bloom_lora'>BLOOM LoRA </option>
-        <option value='bloom_lora_int8'>BLOOM LoRA INT8</option>
+        {Object.keys(modelList).map((key) => (
+          <option value={key}>{modelList[key]}</option>
+        ))}
       </select>
+
+      <label htmlFor='tech'>Choose version: </label>
+      <select
+        style={{ padding: '8px 16px', borderRadius: '8px', marginLeft:'5px' }}
+        name='tech'
+        id='tech'
+        onChange={(e) =>
+          setCode((prev) => ({
+            ...prev,
+            technique: e.target.value,
+          }))
+        }
+      >
+        {Object.keys(trainingTechniques).map((key) => (
+          <option value={key}>{trainingTechniques[key]}</option>
+        ))}
+      </select>
+
       <CodeBlock
       className='row'
       showLineNumbers={false}
@@ -49,9 +89,7 @@ export default function Test(
 from xturing.models import BaseModel
 
 dataset = ${instruction}Dataset('/path/to/dataset')
-model = BaseModel.create('${code}')`} 
-      
-      
+model = BaseModel.create('${finalKey}')`} 
       />
     </div>
   )
