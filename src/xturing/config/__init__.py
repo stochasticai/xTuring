442a67dd767f34e5a8019dc33cc00162299b256e
yiliu30 <yi4.liu@intel.com>
2023-11-01 11:00:34 +0800

    add itrex for cpu
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- src/xturing/config/__init__.py
+++ src/xturing/config/__init__.py
@@ -1,6 +1,10 @@
 import torch
 
 from xturing.utils.interactive import is_interactive_execution
+from xturing.utils.logging import configure_logger
+from xturing.utils.utils import assert_install_itrex
+
+logger = configure_logger(__name__)
 
 # check if cuda is available, if not use cpu and throw warning
 DEFAULT_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -8,8 +12,9 @@ DEFAULT_DTYPE = torch.float16 if DEFAULT_DEVICE.type == "cuda" else torch.float3
 IS_INTERACTIVE = is_interactive_execution()
 
 if DEFAULT_DEVICE.type == "cpu":
-    print("WARNING: CUDA is not available, using CPU instead, can be very slow")
+    logger.warning("WARNING: CUDA is not available, using CPU instead, try running the model with Itrex.")
 
 
-def assert_not_cpu_int8():
-    assert DEFAULT_DEVICE.type != "cpu", "Int8 models are not supported on CPU"
+def assert_cpu_int8_on_itrex():
+    if DEFAULT_DEVICE.type == "cpu":
+        assert_install_itrex()
